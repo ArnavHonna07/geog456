dc85c9bca58f1feae9c86f366061d31252ad4e42
ArnavHonna07 <arnavhonna11@gmail.com>
2026-04-02 16:21:57 -0400

    yes

---
+++ finalProj/data/data.js
@@ -0,0 +1,37 @@
+var data = [
+    {
+        "citiesData": [
+            {
+                "name": "Bisitun Inscription",
+                "lat": 34.39,
+                "long": 47.43
+            },
+            {
+                "name": "Tomb of Cyrus",
+                "lat": 30.19,
+                "long": 53.16
+            },
+            {
+                "name": "Babylon",
+                "lat": 32.53,
+                "long": 44.42
+            },
+            {
+                "name": "Israel",
+                "lat": 31.00,
+                "long": 34.89
+            }
+        ]
+    },
+    {
+        "eventData": [
+            {
+                "points": [34.39, 47.43, 30.19, 53.16],
+                "connection": "From Bisitun to Tomb of Cyrus" 
+            },
+            {
+                "points": []
+            }
+        ]
+    }
+]
